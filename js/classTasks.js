52d8fa0deaef51e24c18046b4f7f09c3c134f67c
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-13 10:05:30 +0300

    Person uzd

--- js/classTasks.js
+++ js/classTasks.js
@@ -32,7 +32,7 @@ class Player {
 }
 
 let pl1 = new Player("James", 35, 180, 75);
-console.group("Players");
+console.groupCollapsed("1. Players");
 
 console.log(pl1.getAge());
 console.log(pl1.getHeight());
@@ -61,7 +61,7 @@ class Calculator {
 
 // susikuriam skaicuotuvo objekta
 let calc = new Calculator();
-console.group("Calculator");
+console.groupCollapsed("2. Calculator");
 console.log("calc.add(10, 5) ->", calc.add(10, 5));
 console.log("calc.multiply(10, 5) ->", calc.multiply(10, 5));
 console.log("calc.divide(10, 5) ->", calc.divide(10, 5));
@@ -73,3 +73,34 @@ console.log("");
 // sukuriant nauja darbuotoja mes paduodam argumentais varda ir pavarde
 // Darbuotojo klase turetu tureti sias savybes
 // name, lastName, fullName, email - name.lastName@compaty.com visos mazosios raides
+
+class Employee {
+  constructor(vardas, pavarde) {
+    this.name = vardas;
+    this.lastname = pavarde;
+    this.fullName = vardas + " " + pavarde;
+    this.email = `${vardas}.${pavarde}@company.com`.toLowerCase();
+    this.getEmail();
+  }
+  getEmail() {
+    this.email2 = `${this.name}.${this.lastname}@sony.com`.toLowerCase();
+  }
+}
+
+let e1 = new Employee("James", "Bond");
+let e2 = new Employee("Jae", "Doe");
+console.groupCollapsed("3. Employee");
+console.log("e1", e1);
+console.log("e2", e2);
+console.groupEnd();
+console.log("");
+
+// 4 sukuri klase Person ir tuscia compare metoda
+// sukurimo metu priskiri varda ir amziu
+// parasyti metoda kuri palygina esama asmeni su kitu ir grazina ar kitas
+// asmuo yra vyresnis jaunes ar to paies amziaus
+
+// const p1 = new Person("Bob", 24);
+// const p2 = new Person("James", 27);
+
+// p1.compare(p2); // -> Bob is younger than James
