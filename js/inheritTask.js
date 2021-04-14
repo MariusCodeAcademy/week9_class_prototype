82866fe57d196cd4b9e6cf24e1774f37bcd41b2a
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-14 12:54:01 +0300

    task done

--- js/inheritTask.js
+++ js/inheritTask.js
@@ -26,13 +26,24 @@ class Person extends LivingThing {
     console.log(`As esu ${this.firstName} ir gimiau ${this.birthYear} metais`);
   }
 }
-const p1 = new Person(1999, "James");
-p1.prisistatymas();
 
 // 3 sukurti klase Student kuri praplecia Person
 // prideti papildoma savybe university
 // course - title
 // course year - kelintas kursas
+class Student extends Person {
+  constructor(birthYear, name, uni, title, courseYear) {
+    super(birthYear, name);
+    this.university = uni;
+    this.courseTitle = title;
+    this.studyYear = courseYear;
+  }
+  prisistatymas() {
+    console.log(
+      `Mano vardas ${this.firstName} as sutudijuoju ${this.university} universiteto, ${this.studyYear} kurse`
+    );
+  }
+}
 
 // 4 sukurti klase Mamal kuri praplecia LivingThing
 // prideti savybe rusis - kokia gyvuno rusis
@@ -46,11 +57,32 @@ class Mamal extends LivingThing {
 //2.1 sukurti 2 asmenis
 // atspausdinti ju gimimo metus
 // atspausdinti prisistatyma
+const p1 = new Person(1999, "James");
+const p2 = new Person(1979, "Bob");
+// atspausdinti gimimo metus
+console.log("p1.birthYear", p1.birthYear);
+console.log("p2.birthYear", p2.birthYear);
+
+[p1, p2].forEach((personObj) => personObj.prisistatymas());
 
 //3.1 sukurti 3 skirtingus studenus
+const students = [
+  new Student(1980, "kevin", "KTU", "WebDev", 1),
+  new Student(1982, "Barbara", "VGTU", "Desin", 2),
+  new Student(1986, "Michale", "KTU", "Python", 3),
+];
 // atspausdinti gimimo metus
+students.forEach((stud) => console.log("stud.birthYear", stud.birthYear));
+
 // perrasyti pasisveikinima ir atspausdinti
+students[0].prisistatymas();
 
 // 3.2 po to kai sukurtas studentas pakeisti kurso skaiciu i didesni.
+students[0].studyYear = 3;
+students[0].prisistatymas();
 
 //4.1 sukurti 2 gyvunus ir atspausdinti gimimo metus
+const mams = [new Mamal(2000, "Tiger"), new Mamal(2010, "Lion")];
+console.log("mams", mams);
+
+mams.forEach((gyv) => console.log("gyv.birthYear", gyv.birthYear));
