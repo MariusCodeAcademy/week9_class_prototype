99a3061b58b0a0024216e20ccfd97f3dfe51cbdb
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-15 09:39:11 +0300

    uzd

--- js/classTasks2.js
+++ js/classTasks2.js
@@ -1,2 +1,41 @@
 "use strict";
 console.log("class tasks 2");
+
+// 1. Sukurti klasę Automobilis
+//   - sukurti konstruktorių, su 3 argumentais:
+//     - marke, modelis, kaina
+
+// 2. Sukurkite 4 skirtingus automobilius pagal klasę Automobilis kintamuosiuose arba masyve:
+
+// 3. Atsispausdinkite car1, car2, car3, car4 kintamuosius.
+// Peržiūrėkite juos konsolėje, panagrinėkite kas yra tie objektai
+
+// 4 sukurti metoda info kuri grazina koks tai yra automobilis string pavidalu pvz "automobilis BMW 3, kaina 4000"
+
+// 5. atspausdinti visu automobiliu modelius
+
+// 6. atspausdinti visu automobiliu markes
+
+// 7. atspausdinti visu automobiliu kainas
+
+// 8. Sukurti metoda padidinti automobilio kainai 100;
+// 8.1 padidinkite pirmo automobilio kaina 300
+// 8.2 padidinkite paskutinio automobilio kaina 500
+
+// 9. Sukurkite metoda nustatyti kaina ivedus skaiciu.
+// pasitikrinkite ar bandomas ivesti yra skaicius ar ne .
+// jei bandomas ivesti nes skaicius praneskite su klaidos pranesimu
+
+// 10 Pridekite Autotomobilio klasei skaitliuka kuris skaiciuoja kiek automobiliu turite sukure
+
+// 11 Sugeneruokite randomId kiekvienam sukurtam automobiliui sukurimo metu ir pridekite kaip savybe
+
+// 12 Sukurkite klase AutoParduotuve
+// sukurimo metu sukurkite savybe parduodamiAutomobiliai tuscia masyva
+
+// 13 pridekite AutoParduotuvei metoda itrauktiAutomobili() kuris priima automobili sukurta pagal Automobilis klase ir
+// prideda ji prie savo parduodamiAutomobiliai masyvo
+
+// 14 Pridekite AutoParduotuves klasei savybe balansas ir prilyginkite ja 0
+// pridekite metoda priktiAutomobili(id) kuris priima id automobilio kuri nori pirkti
+// nupirkimo metu pinigai uz automobili keliauja i balansa o automobilis pasalinamas is parduodamiAutomobiliai masyvo.
