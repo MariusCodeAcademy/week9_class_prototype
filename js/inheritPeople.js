ed08949c7f2d3ca365d19b7e2870ca58257223ad
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-19 12:15:17 +0300

    Peoples

---
+++ js/inheritPeople.js
@@ -0,0 +1,22 @@
+"use strict";
+console.log("inherit people");
+
+// 1  Sukurti klase Person
+// parametru paduosim name, surname, idCode, age, sex
+// sukurti papildoma getteri fullName (vardas pavarde)
+
+// 2 Sukuriam klase Worker kuri paveldi klase Person
+// prideti konstruktoriuje papildomas savybes
+// workingHours(pinas etatas) ir hourlyPay
+// prideti metoda skaiciuoti atlyginimui
+// calcPay(year, month) - ir grazina kiek pinigu gauna zmogus
+// dirbdamas pilnu etatu
+
+// 3 sukurti klase Partner()
+// klase paveldi Person()
+// sukurimo metu pridedam savybe projects kuri bus lygi tusciam
+// masyvui
+// tuscia metoda calcPay()
+// metoda addProject()
+// metodas pasitikrina ar projekas sukurtas pagal Projekto klase
+// ir jei taip ikelia projekta i savo vietine savybe projects
